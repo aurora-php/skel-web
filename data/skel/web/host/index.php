2230e3a1ebdd8e8c4d1bb9c1ffe5af9355619025
Harald Lapp <harald@octris.org>
2014-10-20 23:14:45 +0200

    autoloader is now in libs/

--- data/skel/web/host/index.php
+++ data/skel/web/host/index.php
@@ -20,7 +20,7 @@ namespace {{$namespace}} {
     /**/
 
     // include core web application library
-    require_once(__DIR__ . '/../libs/app/autoloader.class.php');
+    require_once(__DIR__ . '/../libs/autoloader.class.php');
     
     // load application configuration
     $registry = \octris\core\registry::getInstance();
