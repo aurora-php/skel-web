0e39a68346c396747d884aa8617eed3e511d03bf
Harald Lapp <harald@octris.org>
2014-10-20 23:14:31 +0200

    replaced autoloader of web skeleton with improved one

---
+++ data/skel/web/libs/autoloader.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the '{{$vendor}}/{{$module}}' package.
+ *
+ * (c) {{$company}}
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace {{$namespace}} {
+    /**
+     * Autoloader.
+     *
+     * @octdoc      c:libs/main
+     * @copyright   copyright (c) {{$year}} by {{$company}}
+     * @author      {{$author}} <{{$email}}>
+     */
+    class autoloader
+    /**/
+    {
+        /**
+         * Class Autoloader.
+         *
+         * @octdoc  m:autoloader/autoload
+         * @param   string          $class              Class to load.
+         */
+        public static function autoload($class)
+        /**/
+        {
+            if (strpos($class, '{{$namespace}}\\') === 0) {
+                $file = __DIR__ . '/' . str_replace('\\', '/', substr($class, {{length(concat($namespace, '\\'))}})) . '.php';
+            
+                if (file_exists($file)) {
+                    require_once($file);
+                }
+            }
+        }
+    }
+    
+    spl_autoload_register(array('\{{$module}}\autoloader', 'autoload', true, true));
+}
