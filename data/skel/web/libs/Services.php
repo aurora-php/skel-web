aef8ac209dfa9de2ca036b8cd3f725b0847bf203
Harald Lapp <harald@octris.org>
2016-01-04 00:23:50 +0100

    initial commit service delegator template

---
+++ data/skel/web/libs/Services.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the '{{$vendor}}/{{$package}}' package.
+ *
+ * (c) {{$company}}
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace {{$namespace}};
+
+/**
+ * Class defines services.
+ *
+ * @copyright   copyright (c) {{$year}} by {{$company}}
+ * @author      {{$author}} <{{$email}}>
+ */
+class Services extends \Octris\Core\App\Web\Service\Delegator
+{
+    /**
+     * Defined services.
+     *
+     * @type    array
+     */
+    protected $services = array(
+    );
+}
