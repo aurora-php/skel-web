d0b80069bd1a47cc2820afa129123ed6c4e9b398
Harald Lapp <h.lapp@clipdealer.de>
2017-06-01 17:38:49 +0200

    wrap main app in a closure, disable router cache in development environment

--- data/skel/web/host/index.php
+++ data/skel/web/host/index.php
@@ -27,13 +27,15 @@ require_once(__DIR__ . '/../libs/Autoloader.php');
 require_once(__DIR__ . '/../etc/global.php');
 
 // run application
-$router = new \Octris\Core\App\Web\Router\UrlBased(
-    '{{$namespace}}\App\Entry',
-    function(\Octris\Core\App\Web\Router\RuleCollector $r) {
-        $r->addRewrite(['GET', 'POST'], '/');     // default route map to pageRouter
-        $r->addRoute(['GET', 'POST'], '/service/{SERVICE}', new \{{$namespace}}\Services());
-    },
-    OCTRIS_APP_BASE . '/cache/router.cache'
-);
-$app = new App\Main($router);
-$app->run();
+(function() {
+    $router = new \Octris\Core\App\Web\Router\UrlBased(
+        '{{$namespace}}\App\Entry',
+        function(\Octris\Core\App\Web\Router\RuleCollector $r) {
+            $r->addRewrite(['GET', 'POST'], '/');     // default route map to pageRouter
+            $r->addRoute(['GET', 'POST'], '/service/{SERVICE}', new \{{$namespace}}\Services());
+        },
+        ($registry->config['mode'] == 'development' ? null : OCTRIS_APP_BASE . '/cache/router.cache')
+    );
+    $app = new App\Main($router);
+    $app->run();
+})(\Octris\Core\Registry::getInstance());
